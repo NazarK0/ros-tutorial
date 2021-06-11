f83fa5ec88da27602da03f2e5954e4c04c77a261
Nazar Vanivskyi <staticman999@gmail.com>
2021-06-11 15:57:15 +0300

    params

---
+++ dev_ws/build/cpp_parameters/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/nazar/projects/lessons/ros-tutorial/dev_ws/src/cpp_parameters
+# Build directory: /home/nazar/projects/lessons/ros-tutorial/dev_ws/build/cpp_parameters
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
